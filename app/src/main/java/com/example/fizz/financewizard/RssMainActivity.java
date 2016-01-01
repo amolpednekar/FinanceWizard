88f637eb0e330e963365a32a28c82a27d03dd054
Amol Pednekar <amol0202@gmail.com>
2016-01-01 22:14:40 +0530

    Upload 1.4  (RSS Code Clean-up/Re-arrangement)

--- app/src/main/java/com/example/fizz/financewizard/RssMainActivity.java
+++ app/src/main/java/com/example/fizz/financewizard/RssMainActivity.java
@@ -33,13 +33,9 @@ public class RssMainActivity extends AppCompatActivity {
     static final int DIALOG_ERROR_CONNECTION = 1;
     private SwipeRefreshLayout swipeContainer;
 
-    AlertDialog alert1;
-    private AlertDialog.Builder build1;
-    private static boolean isLaunch = true;
     protected FrameLayout frameLayout;
     protected ListView mDrawerList;
     protected DrawerLayout mDrawerLayout;
-    protected ArrayAdapter<String> mAdapter;
     protected ActionBarDrawerToggle mDrawerToggle;
     protected String mActivityTitle;
     protected static int position;
@@ -50,7 +46,7 @@ public class RssMainActivity extends AppCompatActivity {
             setContentView(R.layout.rss_main);
             frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
             mDrawerList = (ListView)findViewById(R.id.navList);
-            mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);//@ activity main
+            mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
             mActivityTitle = "RSS Feed";//string
 
             addDrawerItems();
@@ -64,7 +60,7 @@ public class RssMainActivity extends AppCompatActivity {
                 setContentView(R.layout.rss_main);
                 frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
                 mDrawerList = (ListView)findViewById(R.id.navList);
-                mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);//@ activity main
+                mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
                 mActivityTitle = "RSS Feed";//string
 
                 addDrawerItems();
@@ -98,7 +94,141 @@ public class RssMainActivity extends AppCompatActivity {
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, R.string.exit_press_back_twice_message, Toast.LENGTH_SHORT).show();
     }*/
-    //drawer code begins
+
+    //To check internet connectivity
+    public boolean isOnline(Context c) {
+        ConnectivityManager cm = (ConnectivityManager) c
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+
+        if (ni != null && ni.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    //No internet connnection dialog
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        switch (id) {
+            case DIALOG_ERROR_CONNECTION:
+                AlertDialog.Builder errorDialog = new AlertDialog.Builder(this);
+                errorDialog.setTitle("No internet connection");
+                errorDialog.setMessage("Please try again later");
+                errorDialog.setNeutralButton("OK",
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                AlertDialog errorAlert = errorDialog.create();
+                return errorAlert;
+
+            default:
+                break;
+        }
+        return dialog;
+    }
+
+    // Initialize RSS Feed
+
+    public void addRssFragment() {
+        rss_fragment fragment = new rss_fragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.add(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("fragment_added", true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if (!isOnline(this)) {
+            getMenuInflater().inflate(R.menu.rss_sub_main_nc,menu);
+        }
+        else getMenuInflater().inflate(R.menu.rss_sub_main, menu);
+        return true;
+    }
+
+    void SetRSS()
+    {
+        final CharSequence sources[] = new CharSequence[] {"Personal Finance - Top Stories", "Times Of India - Finance"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select a feed");
+        builder.setItems(sources, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (sources[which] == "Personal Finance - Top Stories") {
+                    Toast.makeText(getApplicationContext(), "Showing Top Stories", Toast.LENGTH_LONG).show();
+                    rss_service.RSS_LINK = "http://feeds.marketwatch.com/marketwatch/pf/";
+                    refresh_feed();
+                } else if (sources[which] == "Times Of India - Finance") {
+                    Toast.makeText(getApplicationContext(), "Showing Finance News", Toast.LENGTH_LONG).show();
+                    rss_service.RSS_LINK = "http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss";
+                    refresh_feed();
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    public void refresh_feed()
+    {
+        setContentView(R.layout.rss_main);
+        frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);//@ activity main
+        mActivityTitle = "FiZZ";//string
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        addRssFragment();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.change_feed) {
+            SetRSS();
+            return true;
+        }
+        if(id==R.id.refresh_rss)
+        {
+            refresh_feed();
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //NavigationDrawer Code Start
     private void addDrawerItems() {
         String[] osArray = {"Cash Flow", "Eco Feed","Goals and Targets","Trends","Reminders"};
 
@@ -169,183 +299,53 @@ public class RssMainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-    //drawer code ends
-    public boolean isOnline(Context c) {
-        ConnectivityManager cm = (ConnectivityManager) c
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo ni = cm.getActiveNetworkInfo();
+    }}
 
-        if (ni != null && ni.isConnected())
-            return true;
-        else
-            return false;
+
+// Navigation Drawer Icons
+class myAdapter extends BaseAdapter {
+    private Context context;
+    String NavListCategories[];
+    int[] images = {R.drawable.cash_flow,R.drawable.rss,R.drawable.goals_targets,R.drawable.trends,R.drawable.reminders};
+
+
+    public myAdapter(Context context){
+        this.context = context;
+        NavListCategories = context.getResources().getStringArray(R.array.NavigationDrawerList);
+
+    }
+    @Override
+    public int getCount() {
+        return NavListCategories.length;
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-        switch (id) {
-            case DIALOG_ERROR_CONNECTION:
-                AlertDialog.Builder errorDialog = new AlertDialog.Builder(this);
-                errorDialog.setTitle("Error");
-                errorDialog.setMessage("No internet connection.");
-                errorDialog.setNeutralButton("OK",
-                        new DialogInterface.OnClickListener() {
-
-                            @Override
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                AlertDialog errorAlert = errorDialog.create();
-                return errorAlert;
-
-            default:
-                break;
-        }
-        return dialog;
-    }
-
-
-    public void addRssFragment() {
-        rss_fragment fragment = new rss_fragment();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-
-        transaction.add(R.id.fragment_container, fragment);
-        transaction.commit();
+    public Object getItem(int position) {
+        return NavListCategories[position];
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean("fragment_added", true);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        if (!isOnline(this)) {
-            getMenuInflater().inflate(R.menu.rss_sub_main_nc,menu);
-        }
-        else getMenuInflater().inflate(R.menu.rss_sub_main, menu);
-        return true;
-    }
-
-
-    class myAdapter extends BaseAdapter {
-        private Context context;
-        String NavListCategories[];
-        int[] images = {R.drawable.cash_flow,R.drawable.rss,R.drawable.goals_targets,R.drawable.trends,R.drawable.reminders};
-
-
-        public myAdapter(Context context){
-            this.context = context;
-            NavListCategories = context.getResources().getStringArray(R.array.NavigationDrawerList);
-
-        }
-        @Override
-        public int getCount() {
-            return NavListCategories.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return NavListCategories[position];
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View row = null;
-            if(convertView == null){
-                LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                row = inflater.inflate(R.layout.custom_row, parent, false);
-            }
-            else{
-                row =convertView;
-            }
-
-            TextView titleTextView =(TextView) row.findViewById(R.id.textViewRow1);
-            ImageView titleImageView = (ImageView) row.findViewById(R.id.imageViewRow1);
-            titleTextView.setText(NavListCategories[position]);
-            titleImageView.setImageResource(images[position]);
-            return row;
-        }
-    }
-
-
-    void SetRSS()
-    {
-        final CharSequence sources[] = new CharSequence[] {"Personal Finance - Top Stories", "Times Of India - Finance"};
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Select a feed");
-        builder.setItems(sources, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (sources[which] == "Personal Finance - Top Stories") {
-                    Toast.makeText(getApplicationContext(), "Showing Top Stories", Toast.LENGTH_LONG).show();
-                    rss_service.RSS_LINK="http://feeds.marketwatch.com/marketwatch/pf/";
-                    ref();
-                } else if (sources[which] == "Times Of India - Finance") {
-                    Toast.makeText(getApplicationContext(), "Showing Finance News", Toast.LENGTH_LONG).show();
-                    rss_service.RSS_LINK="http://timesofindia.feedsportal.com/c/33039/f/533919/index.rss";
-                    ref();
-                }
-            }
-        });
-        builder.show();
-    }
-
-    public void ref()
-    {
-        setContentView(R.layout.rss_main);
-        frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
-        mDrawerList = (ListView)findViewById(R.id.navList);
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);//@ activity main
-        mActivityTitle = "FiZZ";//string
-
-        addDrawerItems();
-        setupDrawer();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        addRssFragment();
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.change_feed) {
-
-            //RssService.RSS_LINK = "";
-            SetRSS();
-            return true;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = null;
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.custom_row, parent, false);
         }
-        if(id==R.id.refresh_rss)
-        {
-            ref();
-            return true;
+        else{
+            row =convertView;
         }
 
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        TextView titleTextView =(TextView) row.findViewById(R.id.textViewRow1);
+        ImageView titleImageView = (ImageView) row.findViewById(R.id.imageViewRow1);
+        titleTextView.setText(NavListCategories[position]);
+        titleImageView.setImageResource(images[position]);
+        return row;
     }
 }
 
+//NavigationDrawer Code End
